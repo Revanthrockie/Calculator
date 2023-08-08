8274d8aeff64a63180c322cc5a0afee3941e4c35
REVANTH <rockierevanth@gmail.com>
2023-08-08 17:10:23 +0530

    idk really

--- script.js
+++ script.js
@@ -1,115 +1,80 @@
-let num1 = '';
-let num2 = '';
+let operandOne = '';
+let operandTwo = '';
 let operator = '';
-let runCalculation = false;
-const ac = document.getElementById('ac');
-const display = document.getElementById('screen');
-const deleteBtn = document.getElementById('delete');
+let doResetScreen = false;
+const buttonbtns = document.querySelectorAll('.btn');
+const operationbtns = document.querySelectorAll('.operator');
+const equalbtn = document.getElementById('equal');
+const clearbtn = document.getElementById('ac');
+const deletebtn = document.getElementById('delete');
+const currentDisplay = document.getElementById('cScreen');
+const previousOperationScreen = document.getElementById('pScreen');
 
-// AC - clears out entire value 
-const reset = document.getElementById('ac');
-reset.addEventListener('click', () => {
-    display.value = '';
-    console.log(display.value)
-    num1 = '';
-    num2 = '';
-    operator = '';
-});
+buttonbtns.forEach((btn) => btn.addEventListener('click', () => assignOperand(btn.textContent)));
+operationbtns.forEach((btn) => btn.addEventListener('click', () => assignOperation(btn.textContent)));
+equalbtn.addEventListener('click' , evaluate);
+clearbtn.addEventListener('click', clear);
+deletebtn.addEventListener('click', backSpace);
 
-function Backspace(){
-    if(runCalculation) return; //Disable Backspace 
+function assignOperand(number){
+    resetScreen()
+    currentDisplay.textContent += number;
+    }
 
-    if(operator){
-        if(num2.length > 0){
-            num2 = num2.substring(0, num2.length - 1);
-            // display.value = display.value.slice(0, -1);
-        }else{
-            operator = "";
-            // display.value = display.value.slice(0, -1);
-        }
-        console.log ('num2: ', num2);
-    }else{
-        if(num1.length > 0){ // 
-        num1 = num1.substring(0, num1.length - 1);
-        // display.value = num1;
-        }
-        // console.log ('num1: ' , num1);
-        display.value = num1 + operator + num2;
+function assignOperation(){
+    console.log('operation');
+}
 
+function resetScreen(){
+    currentDisplay.textContent = '';
+    doResetScreen = false;
+}
+
+function clear(){
+    currentDisplay.textContent = '0';
+    console.log('hello');
+}
+
+function evaluate(){
+    console.log('nothing')
+}
+
+function backSpace(){
+    console.log('hi');
+}
+
+
+
+
+
+
+function operate(operator, a , b){
+    a = Number(a);
+    b = Number(b);
+
+    switch(operator){
+        case '+':
+            add(a , b);
+        break;
+
+        case '-':
+            sub(a, b);
+        break;
+        
+        case 'x':
+            mul(a , b);
+        break;
+
+        case '/':
+            div(a, b);
+        break;
+
+        default:
+            console.log('operator not founc');
+        break;
     }
 }
 
-
-deleteBtn.addEventListener('click', Backspace);
-// let isOperator = false;
-
-
-
-// let arr =[];
-function pushValue(e){
-    const value = e.target.textContent;
-
-    if(runCalculation){
-        num1 = value;
-        num2 = '';
-        operator = '';
-        display.value = value;
-        runCalculation = false;
-    } else {
-        if(!operator){
-            num1 += value;
-        }else{
-            num2 += value;
-        }
-        // display.value += value;
-    }
-    display.value = num1 + operator + num2;
-}
-
-// function to check when the operator is clicked to assign num1 and num2 respectively
-function checkOperatorClicked(e){
-    const value = e.target.textContent;
-    if( value === '='){
-        operation();
-    } else {
-        operator = value;
-        // display.value += value;
-    } 
-    display.value = num1 + operator + num2;
-}
-       
-
-
-function operation(){
-    let num1Value = parseFloat(num1).toFixed(2);
-    console.log(num1Value);
-    console.log(operator);
-    let num2Value = parseFloat(num2).toFixed(2);
-    console.log(num2Value);
-
-    // if(isNaN(num1Value) || isNaN(num2Value) || !operator){
-    //     display.value = "ERROR";
-    //     return;
-    // };
-
-    if(operator === '+'){
-            display.value = add(num1Value, num2Value);
-    }else if(operator === '-'){
-            display.value = sub(num1Value, num2Value);
-    }else if(operator === '*'){
-            display.value = mul(num1Value, num2Value).toFixed(2);
-    }else if(operator === '/'){
-            display.value = div(num1Value, num2Value).toFixed(2);
-    }
-
-    num1 = display.value;
-    num2 = '';
-    operator = '';
-
-    runCalculation = true;
-}
-    
-   
 function add(a , b){
     return a + b;
 }
@@ -123,23 +88,5 @@ function mul(a , b){
 }
 
 function div(a , b){
-    return b === 0 ? "OOPS" : a / b;
+    return a / b;
 }
-  
-const button = document.querySelectorAll('.btn');
-button.forEach(btn => btn.addEventListener('click', pushValue));
-// button.forEach(btn => btn.addEventListener('click', checkOperatorClicked ));
-
-
-const operate = document.querySelectorAll('.operator');
-operate.forEach(btn => btn.addEventListener('click', checkOperatorClicked));
-// operate.forEach(btn => btn.addEventListener('click', checkOperate));
-
-const enter = document.getElementById('equal');
-enter.addEventListener('click', operation);
-
-
-
-
-
-
